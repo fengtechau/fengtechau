83c6d69108ce34165f05d2574293e3cc5509d484
fengtechau <96329838+fengtechau@users.noreply.github.com>
2023-11-25 14:50:00 +1100

    Update to version 17

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'root'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('root');
+    expect(app.title).toEqual('FENG TECH');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('root app is running!');
-  });
 });
